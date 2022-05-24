99fe7ee0f0c44c16711a61c66ec366b464cbb2aa
49 <63515275+4-9@users.noreply.github.com>
2022-05-24 15:51:03 +0100

    lint admin

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,59 +1,69 @@
-
+""" Admin cog for admin commands """
 
 # nextcord stuff
-from logging import exception
 import nextcord
 from nextcord.ext import commands
-from nextcord import Integration, SlashOption, CommandOption
+from nextcord import Integration, SlashOption
 
 # sqlalchemy
-from sqlalchemy import func, select, update
+from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm.exc import NoResultFound
 
-from cogbot import logger, cogGuild, db, MembersInfo, bot
+# pylint: disable=import-error
+from cogbot import logger, cogGuild, db, MembersInfo
 
 if __name__ == "__main__":
+    # pylint: disable=pointless-statement
     exit
 
-def getAllId():
+def get_all_id():
+    """
+    Returns all discord_ids from the db
+    """
     result = {}
     statement = select(MembersInfo.discord_id)
     try:
         result = db.execute(statement).all()
         return result
-    except Exception as e:
-        logger.error(f"Error: {e}")
+    except IntegrityError as error:
+        logger.error("Error: %s", error)
         return None
 
 class AdminCog(commands.Cog):
+    """
+    Admin cog
+    """
 
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
-    
+
     @nextcord.slash_command(
         name="admin",
         description="Administrator commands",
         default_permission=False,
         guild_ids=cogGuild,
-        
+
     )
 
+    # pylint: disable=no-self-use
     async def promotion(
-        self, 
+        self,
         interaction: Integration,
         command: str = SlashOption(
             name="command",
             description="...",
             choices={"Purge old users from the db": "purge"},
             required=True)):
+        """
+        promotion?
+        """
 
         if interaction.user.id == 136929317316853760:
             # await interaction.send(ephemeral=True, content=f"Fuck")
             if command == "purge":
-                await interaction.send(ephemeral=True, content=f"PURGE THEM")
-                # print(getAllId())
-                # all_db_members = getAllId()
+                await interaction.send(ephemeral=True, content="PURGE THEM")
+                # print(get_all_id())
+                # all_db_members = get_all_id()
                 # intents = nextcord.Intents(messages=True, guilds=True)
                 # intents.members = True
                 # all_discord_members = bot.guilds[0].members
@@ -71,7 +81,10 @@ class AdminCog(commands.Cog):
                 #     print(member)
 
         else:
-            await interaction.send(ephemeral=True, content=f"49 only command")
+            await interaction.send(ephemeral=True, content="49 only command")
 
 def setup(bot):
+    """
+    Magic
+    """
     bot.add_cog(AdminCog(bot))
