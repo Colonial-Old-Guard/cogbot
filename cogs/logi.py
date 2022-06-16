2fe3848d86d408508e94c2bde2e65cf7b39e477a
49 <63515275+4-9@users.noreply.github.com>
2022-06-16 16:30:50 +0100

    less spam

--- cogs/logi.py
+++ cogs/logi.py
@@ -61,42 +61,57 @@ class LogiCog(commands.Cog):
         logi_role = interaction.guild.get_role(int(role))
 
         if add_or_remove == "add" and is_logi_lead(interaction.user):
-            try:
-                logger.info(f"{interaction.user.nick}|{interaction.user.id}"\
-                    f" adding {logi_role.name} to {member.nick}|{member.id}")
-                await member.add_roles(logi_role)
-                await logi_log_channel.send(content=f"{interaction.user.mention}"\
-                    f" granted {logi_role.mention} to {member.mention}"
+            if logi_role.id in member.roles:
+                logger.info(f"{member.nick}|{member.id}"\
+                    f" already has the {logi_role.name}|{logi_role.id} role")
+                await interaction.response.send_message(ephemeral=True,content=
+                f"{member.mention} already has the {logi_role.mention} role"
                 )
-                await interaction.response.send_message(ephemeral=True,
-                    content=f"Granted {member.mention} the {logi_role.mention} role")
-            except Forbidden as error:
-                print(f"no permissions: {error}")
-                logger.error(f"Incorrect permissions adding roles to "
-                    f"{member.name}|{member.id}: {error}")
-            except HTTPException as error:
-                print(f"Other error: {error}")
-                logger.error(f"HTTP error adding roles to "
-                    f"{member.name}|{member.id}: {error}")
+            else:
+                try:
+                    logger.info(f"{interaction.user.nick}|{interaction.user.id}"\
+                        f" adding {logi_role.name} to {member.nick}|{member.id}")
+                    await member.add_roles(logi_role)
+                    await logi_log_channel.send(content=f"{interaction.user.mention}"\
+                        f" granted {logi_role.mention} to {member.mention}"
+                    )
+                    await interaction.response.send_message(ephemeral=True,
+                        content=f"Granted {member.mention} the {logi_role.mention} role")
+                except Forbidden as error:
+                    print(f"no permissions: {error}")
+                    logger.error(f"Incorrect permissions adding roles to "
+                        f"{member.name}|{member.id}: {error}")
+                except HTTPException as error:
+                    print(f"Other error: {error}")
+                    logger.error(f"HTTP error adding roles to "
+                        f"{member.name}|{member.id}: {error}")
 
         if add_or_remove == "remove" and is_logi_lead(interaction.user):
-            try:
-                logger.info(f"{interaction.user.nick}|{interaction.user.id}"\
-                    f" removing roles to {member.nick}|{member.id}")
-                await member.remove_roles(logi_role)
-                await logi_log_channel.send(content=f"{interaction.user.mention}"\
-                    f" removed {logi_role.mention} to {member.mention}"
+            if logi_role.id in member.roles:
+                try:
+                    logger.info(f"{interaction.user.nick}|{interaction.user.id}"\
+                        f" removing roles to {member.nick}|{member.id}")
+                    await member.remove_roles(logi_role)
+                    await logi_log_channel.send(content=f"{interaction.user.mention}"\
+                        f" removed {logi_role.mention} to {member.mention}"
+                    )
+                    await interaction.response.send_message(ephemeral=True,
+                        content=f"Removed {member.mention} from  the {logi_role.mention} role")
+                except Forbidden as error:
+                    print(f"no permissions: {error}")
+                    logger.error(f"Incorrect permissions adding roles to "
+                        f"{member.name}|{member.id}: {error}")
+                except HTTPException as error:
+                    print(f"Other error: {error}")
+                    logger.error(f"HTTP error adding roles to "
+                        f"{member.name}|{member.id}: {error}")
+            else:
+                logger.info(f"{member.nick}|{member.id}"\
+                    f" does not have the {logi_role.name}|{logi_role.id} role")
+                await interaction.response.send_message(ephemeral=True,content=
+                f"{member.mention} does not have the {logi_role.mention} role"
                 )
-                await interaction.response.send_message(ephemeral=True,
-                    content=f"Removed {member.mention} from  the {logi_role.mention} role")
-            except Forbidden as error:
-                print(f"no permissions: {error}")
-                logger.error(f"Incorrect permissions adding roles to "
-                    f"{member.name}|{member.id}: {error}")
-            except HTTPException as error:
-                print(f"Other error: {error}")
-                logger.error(f"HTTP error adding roles to "
-                    f"{member.name}|{member.id}: {error}")
+
 
 def setup(bot):
     """
